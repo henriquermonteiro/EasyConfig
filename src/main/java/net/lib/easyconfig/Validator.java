10c5912ab0d2ed033600ca46700b11fb871f1568
henriquermonteiro <henrique.r.monteiro@hotmail.com>
2018-08-21 08:17:01 -0300

    Create Validator.java

---
+++ src/main/java/net/lib/easyconfig/Validator.java
@@ -0,0 +1,37 @@
+package net.lib.easyconfig;
+
+import java.util.Properties;
+
+public final class Validator{
+    public static boolean validate(Properties model, Properties prop, boolean strict){
+        int model_count = 0;
+    
+        for(Object model_key in model.getKeyValues()){
+            String conditions = model.get(model_key).toString();
+            
+            Object prop_value = prop.get(model_key);
+            
+            if(conditions.contains("req") && prop_value == null){
+                return false;
+            }
+            
+            if(conditions.contains("not_empty") && prop_value.toString().equals("")){
+                return false;
+            }
+            
+            int index = conditions.indexOf("matches[/");
+            if(index != -1){
+                int index_end = conditions.indexOf("/]-");
+                string regex = conditions.substring(index + 8, index_end);
+                
+                if(!prop_value.toString().matches(regex)){
+                    return false;
+                }
+            }
+            
+            model_count++;
+        }
+        
+        return (strict ? model_count == prop.getKeyValues().lenght : true);
+    }
+}
